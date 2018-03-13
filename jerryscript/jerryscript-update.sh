dbf63d616777bafa5a8459de7f39a1c2193a7946
Renata Hodovan <reni@inf.u-szeged.hu>
2018-03-13 12:28:13 +0100

    Add JerryScript configurations.

---
+++ jerryscript/jerryscript-update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copyright (c) 2018 Renata Hodovan, Akos Kiss.
+#
+# Licensed under the BSD 3-Clause License
+# <LICENSE-BSD-3-Clause.rst or https://opensource.org/licenses/BSD-3-Clause>.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+git reset --hard origin/master
+git pull origin master
+rm -rf build
+
+# $1 is the build command to execute.
+eval $1
