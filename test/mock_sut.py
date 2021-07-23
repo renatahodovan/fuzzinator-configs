5238d6ab2085a8fd08dfe422ce1154c1ba7a0c60
Hodovan Renata <reni@inf.u-szeged.hu>
2021-07-23 14:17:38 +0200

    Convert test input from bytes to string
    As a result of a recent change, Fuzzinator uses strings to store
    metadata and string regexes to process them.

--- test/mock_sut.py
+++ test/mock_sut.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2020-2021 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -7,5 +7,5 @@
 
 def MockSubprocessCall(test, field='stderr', **kwargs):
 
-    with open(test, 'rb') as f:
+    with open(test, 'r') as f:
         return {field: f.read()}
